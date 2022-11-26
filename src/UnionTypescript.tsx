6b8bfc9712ce67dba759e1055ea9780cdf2882a6
渡邉将太 <shotawebcoding@gmail.com>
2022-11-26 17:31:11 +0900

    Update UnionTypescript.tsx

--- src/UnionTypescript.tsx
+++ src/UnionTypescript.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { number } from 'yargs';
 
 const functionTypescript = () => {
 
@@ -11,7 +12,22 @@ const functionTypescript = () => {
     console.log('Result' + num);
   }
 
-  printResult(add(5, 12));
+  function addAndHandle(n1: number, n2: number, cb: (num: number) => void) { 
+    const result = n1 + n2;
+    cb(result);
+  }
+
+  addAndHandle(10, 20, (result) => { 
+    console.log(result)
+  });
+
+  let combineValues: (a: number, b: number) => number;
+
+  combineValues = add;
+
+  console.log(combineValues(8, 8));
+
+
 
   return (
     <div>
