bdd9f6b668280f47a79f60324b8996b9b6913c59
渡邉将太 <shotawebcoding@gmail.com>
2022-11-26 15:42:44 +0900

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ObjectTypescript from './ObjectTypescript';
 
 function App() {
+
   return (
     <div className="App">
-     
+     <ObjectTypescript />
     </div>
   );
 }
---
+++ src/ObjectTypescript.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ObjectTypescript = () => {
+
+    const person = {
+        name: 'yota',
+        age: 30
+    }
+
+    console.log(person);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ObjectTypescript
