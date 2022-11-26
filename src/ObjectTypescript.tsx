45c4d9ce4d567b02c0acb934ad2abc3867bfa50d
渡邉将太 <shotawebcoding@gmail.com>
2022-11-26 15:56:40 +0900

    Update ObjectTypescript.tsx

--- src/ObjectTypescript.tsx
+++ src/ObjectTypescript.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const ObjectTypescript = () => {
 
-    const person= {
+    const person: { name: string; age: number; hobbies: string[]; role: [ number, string ]; } = {
         name: 'yota',
         age: 30,
-        hobbies: ['Sports','Cooking']
+        hobbies: ['Sports', 'Cooking'],
+        role: [2, 'author'],
     }
 
     let favoriteActivetues: string[];
